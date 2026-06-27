52b4e2a8b481a80d17252f049f5f780106b5ab4d
Elimeowww <tangsan1385@gmail.com>
2026-06-27 21:15:01 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,14 +3,36 @@ from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, Con
 
 TOKEN = "8871850096:AAG7WEcfdR7Zg-BbmQuBqJdM2BpLtYroMl8"
 
+# 📌 کانال اجباری (حتماً @ بگذار)
+CHANNEL_USERNAME = "@YourChannel"
+
 FILES = {
     "manhwa1": "FILE_ID_1",
     "manhwa25": "BQACAgQAAxkBAANTakAIpqeymmDk1IQAAXbDrxEe1m1fAAI8GAACtFn4UarzHCrgfAS-PAQ",
     "manhwa3": "FILE_ID_3"
 }
 
+# 🔍 چک عضویت کاربر
+async def check_membership(bot, user_id):
+    try:
+        member = await bot.get_chat_member("@gaptestes", user_id)
+        return member.status in ["member", "creator", "administrator"]
+    except:
+        return False
+
 # 🚀 استارت
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    bot = context.bot
+
+    is_member = await check_membership(bot, user_id)
+
+    if not is_member:
+        await update.message.reply_text(
+            f"❌ برای استفاده از ربات باید عضو کانال زیر بشی:\n\n{CHANNEL_USERNAME}\n\nبعد دوباره /start بزن"
+        )
+        return
+
     args = context.args
 
     if args:
@@ -18,30 +40,27 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         if key in FILES:
             await update.message.reply_text("📦 در حال ارسال فایل...")
-            await update.message.reply_document(document=FILES[key])
+
+            await update.message.reply_document(
+                document=FILES[key]
+            )
         else:
             await update.message.reply_text("❌ فایل پیدا نشد")
-    else:
-        await update.message.reply_text("سلام 👋\nفایل بفرست تا ID بدم")
 
-# 🟢 گرفتن file_id (نسخه دقیق‌تر)
+    else:
+        await update.message.reply_text("سلام 👋\nلینک فایل رو بزن")
+
+# 🟢 گرفتن file_id
 async def get_file_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message.document:
-        file_id = update.message.document.file_id
-
         await update.message.reply_text(
-            f"📦 FILE_ID:\n{file_id}"
+            f"📦 FILE_ID:\n{update.message.document.file_id}"
         )
-
-    else:
-        await update.message.reply_text("❌ این فایل نیست")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-
-    # ⚠️ این مهم‌ترین خطه
     app.add_handler(MessageHandler(filters.Document.ALL, get_file_id))
 
     print("Bot is running...")
