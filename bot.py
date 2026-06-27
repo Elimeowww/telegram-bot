77b80eeb93d83fbb76fe8d611957d8220b3dae42
Elimeowww <tangsan1385@gmail.com>
2026-06-27 20:49:51 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,20 +3,36 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = "8871850096:AAG7WEcfdR7Zg-BbmQuBqJdM2BpLtYroMl8"
 
+# اینجا فایل‌هات رو می‌ذاری
+FILES = {
+    "manhwa1": "FILE_ID_1",
+    "manhwa2": "FILE_ID_2",
+    "chapter3": "FILE_ID_3"
+}
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     args = context.args
 
-    if args and args[0] == "manhwa1":
-        await update.message.reply_text("📦 فایل در حال ارسال...")
+    if args:
+        key = args[0]
 
-        await update.message.reply_document(
-            document=open("Ch01_ Song Of The Wasteland Farsi-{AoiSekai}", "rb")
-        )
+        if key in FILES:
+            await update.message.reply_text("📦 در حال ارسال فایل...")
+
+            await update.message.reply_document(
+                document=FILES[key]
+            )
+        else:
+            await update.message.reply_text("❌ فایل پیدا نشد")
     else:
         await update.message.reply_text("سلام 👋")
 
-app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
 
-print("Bot is running...")
-app.run_polling()
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
