1fe75abf7591f13aa01cc6c4a8410ed31d8f2460
Elimeowww <tangsan1385@gmail.com>
2026-06-29 16:01:03 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,63 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
 TOKEN = "8871850096:AAFfAuWjR1BPHTOkCeNxLuFtH2lpxTmJpck"
+CHANNEL = "@femboycuteir"
+
+# فایل‌ها
+FILES = {
+    "manhwa25": "BQACAgQAAxkBAAIBA2pBEfZBoO8ww5EhmvrTXM8tlxsXAAI3HwACV-UJUnJzplB_2dIGPAQ"
+
+    "manwha26": "BQACAgQAAxkBAAIBumpCZQqJxF2l4g1kthuil1wZdp1aAAKDIAACLqsQUmnSXRv_AekpPAQ"
+}
+
+
+async def is_member(bot, user_id):
+    """چک کردن عضویت"""
+    try:
+        member = await bot.get_chat_member(CHANNEL, user_id)
+        return member.status in ["member", "creator", "administrator"]
+    except:
+        return False
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("سلام! فایل بفرست")
-
-async def handle_file(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message.document:
-        file_id = update.message.document.file_id
-        file_name = update.message.document.file_name
+    """استارت با لینک"""
+    user_id = update.effective_user.id
+    bot = context.bot
+    args = context.args
+    
+    # اگر لینک با پارامتر باشه
+    if args:
+        file_key = args[0]
         
-        await update.message.reply_text(
-            f"✅ فایل دریافت شد!\n\n"
-            f"📝 نام: {file_name}\n"
-            f"🔑 کد: {file_id}"
-        )
+        # چک عضویت
+        if not await is_member(bot, user_id):
+            keyboard = [
+                [InlineKeyboardButton("📢 عضویت در کانال", url=f"https://t.me/{CHANNEL.replace('@','')}")]
+            ]
+            await update.message.reply_text(
+                "❌ اول عضو کانال شو، بعدش دوباره لینک رو بزن",
+                reply_markup=InlineKeyboardMarkup(keyboard)
+            )
+            return
+        
+        # ارسال فایل
+        if file_key in FILES:
+            await update.message.reply_document(FILES[file_key])
+        else:
+            await update.message.reply_text("❌ فایل پیدا نشد")
+    else:
+        # استارت بدون پارامتر
+        pass
 
-if __name__ == "__main__":
+
+if name == "main":
     app = ApplicationBuilder().token(TOKEN).build()
     
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.Document.ALL, handle_file))
     
     app.run_polling()
